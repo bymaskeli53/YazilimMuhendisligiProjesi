df7514c4d85c11749ed84fc5775823ccd91cbe05
Huseyin Aslan <btdhaslan@gmail.com>
2022-11-29 11:42:12 +0300

    separate users each other and improve interface login page

---
+++ Project/indexx.js
@@ -0,0 +1,12 @@
+// function show_kadin() {
+//   document.getElementById() diğerlerini none yapmak lazım
+//   document.getElementById("show_erkek_products").style.display = "none";
+//   document.getElementById("show_cocuk_products").style.display = "none";
+//   document.getElementById("show_guzellik_products").style.display = "none";
+//   var x = document.getElementById("show_kadin_products");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
